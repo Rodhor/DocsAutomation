a0346ef7546ab088a310a7db4e38c7378a202fb2
Rodhor <chris.rahbek.nielsen@gmail.com>
2023-10-22 16:21:19 +0200

    added Icon to Setupfile and add requirementsfile

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-build_options = {'packages': [], 'excludes': ["gi", "gtk", "PyQt4", "PyQt5", "PyQt6", "PySide2", "PySide6", "shiboken2", "shiboken6", "wx"]}
+includefiles = ["logo.ico"]
+build_options = {'packages': [], 'excludes': ["gi", "gtk", "PyQt4", "PyQt5", "PyQt6", "PySide2", "PySide6", "shiboken2", "shiboken6", "wx"], 'include_files':includefiles}
 
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
@@ -10,10 +11,9 @@ base = 'Win32GUI' if sys.platform=='win32' else None
 executables = [
     Executable('Viona.py', base=base, target_name = 'App')
 ]
-include_files =[("logo.png")]
 
-setup(name='App',
+setup(name='Viona App',
       version = '1',
-      description = 'A viona app',
+      description = 'A Viona App',
       options = {'build_exe': build_options},
       executables = executables)
